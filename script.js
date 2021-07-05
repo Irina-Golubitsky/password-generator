e9f745b8856e95fb2c55923044a8b9eac3a598a4
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-07-04 19:37:59 -0700

    javascript: check password length input

--- script.js
+++ script.js
@@ -4,12 +4,18 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  let inLength = parseInt(prompt("Please enter password length (number between 8 and 128)", 8));
+  if ((inLength>=8)&&(inLength<=128)){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+} else {
+  alert("Error! Password length should be an integer number between 8 and 128");
+  }
 
 }
 
